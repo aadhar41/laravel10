b2953d76291e15061d1c9fcbdfbdf73ae8260544
Aadhar gaur <aadhar41@gmail.com>
2023-12-23 16:52:07 +0530

    010 Styling Post List Page

--- resources/views/posts/partials/post.blade.php
+++ resources/views/posts/partials/post.blade.php
@@ -1,13 +1,12 @@
-@if ($loop->even)
-    <p>{{ $key }} - {{ $post->title }}</p>
-@else
-    <p class="lead">
-        {{ $key }} - {{ $post->title }}
-    </p>
-@endif
+<a name="" id="" class="badge text-muted" href="{{ route('posts.show', ['post' => $post->id]) }}"
+    role="button">
+    <h3> {{ $post->title }}</h3>
+</a>
 
-<div>
-    <form action="{{ route('posts.destroy', ['post' => $post->id]) }}" method="POST">
+<div class="mb-3">
+    <a name="" id="" class="btn btn-primary" href="{{ route('posts.edit', ['post' => $post->id]) }}"
+        role="button"><i class="fas fa-edit"></i>Edit</a>
+    <form action="{{ route('posts.destroy', ['post' => $post->id]) }}" class="d-inline" method="POST">
         @csrf
         @method('DELETE')
         <button type="submit" class="btn btn-danger">Delete!</button>
