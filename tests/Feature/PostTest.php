a3b8b753cc03979323d8d5bc5098104b5d673130
Aadhar gaur <aadhar41@gmail.com>
2023-12-23 19:31:26 +0530

    008 Testing delete() action (model deletion)

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -17,17 +17,12 @@ class PostTest extends TestCase
     public function test_no_blog_posts_when_nothing_in_the_database(): void
     {
         $response = $this->get('/posts');
-
         $response->assertSeeText('No Blog Post.');
     }
 
     public function test_see_one_blog_post_when_there_is_one(): void
     {
-        // Arrange
-        $post = new BlogPost();
-        $post->title = 'New title';
-        $post->content = 'Content of the blog post.';
-        $post->save();
+        $this->createDummyBlogPost();
 
         // Act
         $response = $this->get('/posts');
@@ -52,5 +47,79 @@ class PostTest extends TestCase
             ->assertSessionHas('status');
 
         $this->assertEquals(session('status'), 'The blog post created!');
+    }
+
+    public function test_store_fail(): void
+    {
+        $params = [
+            'title' => 'x',
+            'content' => 'x',
+        ];
+
+        $this->post('/posts', $params)
+            ->assertStatus(302)
+            ->assertSessionHas('errors');
+
+        $messages = session('errors')->getMessages();
+
+        $this->assertEquals($messages['title'][0], 'The title field must be at least 5 characters.');
+        $this->assertEquals($messages['content'][0], 'The content field must be at least 10 characters.');
+    }
+
+    public function test_update_valid_post(): void
+    {
+        $post = $this->createDummyBlogPost();
+
+        $this->assertDatabaseHas('blog_posts', [
+            'title' => 'New title',
+        ]);
+
+        $params = [
+            'title' => 'A new named title.',
+            'content' => 'Content was changed.',
+        ];
+
+        $this->put("/posts/{$post->id}", $params)
+            ->assertStatus(302)
+            ->assertSessionHas('status');
+
+        $this->assertEquals(session('status'), 'The blog post updated!');
+
+        $this->assertDatabaseMissing('blog_posts', [
+            'title' => 'New title',
+        ]);
+
+        $this->assertDatabaseHas('blog_posts', [
+            'title' => 'A new named title.',
+        ]);
+    }
+
+    public function test_delete(): void
+    {
+        $post = $this->createDummyBlogPost();
+
+        $this->assertDatabaseHas('blog_posts', [
+            'title' => 'New title',
+        ]);
+
+        $this->delete("/posts/{$post->id}")
+            ->assertStatus(302)
+            ->assertSessionHas('status');
+
+        $this->assertEquals(session('status'), 'Blog post deleted!');
+
+        $this->assertDatabaseMissing('blog_posts', [
+            'title' => 'New title',
+        ]);
+    }
+
+    public function createDummyBlogPost(): BlogPost
+    {
+        // Arrange
+        $post = new BlogPost();
+        $post->title = 'New title';
+        $post->content = 'Content of the blog post.';
+        $post->save();
+        return $post;
     }
 }
