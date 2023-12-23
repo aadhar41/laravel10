19d220c6797af91f4d191737d15721e2160fea50
Aadhar gaur <aadhar41@gmail.com>
2023-12-23 17:47:11 +0530

    002 Testing configuration and environment

--- resources/views/posts/partials/post.blade.php
+++ resources/views/posts/partials/post.blade.php
@@ -1,20 +1,27 @@
-<a name="" id="" class="badge text-muted" href="{{ route('posts.show', ['post' => $post->id]) }}"
-    role="button">
-    <h3>
-        {{ $post->title }}
-        @if (now()->diffInMinutes($post->created_at) < 5)
-            <span class="badge rounded-pill bg-primary mx-2">New !</span>
-        @endif
-    </h3>
-
-</a>
-
-<div class="mb-3">
-    <a name="" id="" class="btn btn-primary" href="{{ route('posts.edit', ['post' => $post->id]) }}"
-        role="button"><i class="fas fa-edit"></i>Edit</a>
-    <form action="{{ route('posts.destroy', ['post' => $post->id]) }}" class="d-inline" method="POST">
-        @csrf
-        @method('DELETE')
-        <button type="submit" class="btn btn-danger">Delete!</button>
-    </form>
+<div class="card mb-2">
+    <div class="card-title">
+        <h4>
+            <a name="" id="" class="badge text-muted"
+                href="{{ route('posts.show', ['post' => $post->id]) }}" role="button">
+                <h3>
+                    {{ $post->title }}
+                    @if (now()->diffInMinutes($post->created_at) < 5)
+                        <span class="badge rounded-pill bg-primary mx-2">New !</span>
+                    @endif
+                </h3>
+            </a>
+        </h4>
+    </div>
+    <div class="card-body">
+        <div class="mb-1">
+            <a name="" id="" class="btn btn-primary"
+                href="{{ route('posts.edit', ['post' => $post->id]) }}" role="button"><i
+                    class="fas fa-edit"></i>Edit</a>
+            <form action="{{ route('posts.destroy', ['post' => $post->id]) }}" class="d-inline" method="POST">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn btn-danger">Delete!</button>
+            </form>
+        </div>
+    </div>
 </div>
