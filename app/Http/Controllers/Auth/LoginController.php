a3b7a2f8e94d48b6222fbd4ddc27af65cc7ed75c
Aadhar gaur <aadhar41@gmail.com>
2024-01-01 16:22:09 +0530

    008 Log-in form with Remember Me feature

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -37,4 +37,15 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+
+    /**
+     * The function returns the string 'email' as the username.
+     *
+     * @return string the string 'email'.
+     */
+    public function username()
+    {
+        return 'email';
+    }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,47 +11,47 @@
                             @csrf
 
                             <div class="form-group mb-2">
-                                <label for="name">Name</label>
+                                <label for="name">{{ __('Name') }}</label>
                                 <input type="text" class="form-control @error('name') is-invalid @enderror"
-                                    name="name" id="name" aria-describedby="helpId" placeholder="Name"
-                                    value="{{ old('name') }}" autocomplete="off">
+                                    name="name" id="name" aria-describedby="helpId"
+                                    placeholder="{{ __('Name') }}" value="{{ old('name') }}" autocomplete="off">
                                 @error('name')
                                     <small id="helpId" class="form-text invalid-feedback">{{ $message }}</small>
                                 @enderror
                             </div>
 
                             <div class="form-group mb-2">
-                                <label for="email">E-mail</label>
+                                <label for="email">{{ __('E-mail') }}</label>
                                 <input type="email" class="form-control @error('email') is-invalid @enderror"
-                                    name="email" id="email" aria-describedby="emailHelpId" placeholder="E-Mail"
-                                    value="{{ old('email') }}" autocomplete="off">
+                                    name="email" id="email" aria-describedby="emailHelpId"
+                                    placeholder="{{ __('E-mail') }}" value="{{ old('email') }}" autocomplete="off">
                                 @error('email')
                                     <small id="emailHelpId" class="form-text invalid-feedback">{{ $message }}</small>
                                 @enderror
                             </div>
 
                             <div class="form-group mb-2">
-                                <label for="pasword">Password</label>
+                                <label for="pasword">{{ __('Password') }}</label>
                                 <input type="password" class="form-control @error('password') is-invalid @enderror"
-                                    name="password" id="password" placeholder="Password" autocomplete="off" />
+                                    name="password" id="password" placeholder="{{ __('Password') }}" autocomplete="off" />
                                 @error('password')
                                     <small id="emailHelpId" class="form-text invalid-feedback">{{ $message }}</small>
                                 @enderror
                             </div>
 
                             <div class="form-group mb-2">
-                                <label for="password_confirmation">Password Confirmation</label>
+                                <label for="password_confirmation">{{ __('Confirm Password') }}</label>
                                 <input type="password"
                                     class="form-control @error('password_confirmation') is-invalid @enderror"
                                     name="password_confirmation" id="password_confirmation"
-                                    placeholder="Password Confirmation" autocomplete="off" />
+                                    placeholder="{{ __('Confirm Password') }}" autocomplete="off" />
                                 @error('password_confirmation')
                                     <small id="emailHelpId" class="form-text text-danger">{{ $message }}</small>
                                 @enderror
                             </div>
 
                             <div class="row p-2">
-                                <button type="submit" class="btn btn-block btn-primary">Register</button>
+                                <button type="submit" class="btn btn-block btn-primary">{{ __('Register') }}</button>
                             </div>
                         </form>
                     </div>
