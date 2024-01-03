e96471779658cf6082e66fdc118c1fbfc9c230d2
Aadhar gaur <aadhar41@gmail.com>
2024-01-03 09:23:15 +0530

    Contact Page Form UI Updated

--- resources/views/home/contact.blade.php
+++ resources/views/home/contact.blade.php
@@ -3,8 +3,8 @@
 
 @section('content')
     <div class="jumbotron">
-        <h1 class="display-3">Contact Page</h1>
-        <p>This is the content of the contact page.</p>
+        <h1 class="display-3 mx-2">Contact Page</h1>
+        <p class="mx-3">This is the content of the contact page.</p>
         @can('home.secret')
             <div class="alert alert-primary" role="alert">
                 <a href="{{ route('secret') }}" class="alert-link">Special Contact Details</a>
@@ -14,23 +14,24 @@
             <div class="wrap-contact2">
                 <form action="" method="post">
                     @csrf
-                    <div class="form-group">
-                        <label for="">Name</label>
-                        <input type="text" class="form-control" name="" id="" aria-describedby="helpId"
-                            placeholder="">
-                        <small id="helpId" class="form-text text-muted">Help text</small>
+
+                    <div class="form-floating mb-3">
+                        <input type="text" class="form-control" name="name" id="name"
+                            placeholder="{{ __('Name') }}" value="{{ old('name') }}" autocomplete="off" />
+                        <label for="name">{{ __('Name') }}</label>
                     </div>
 
-                    <div class="form-group">
-                        <label for="">E-Mail</label>
-                        <input type="email" class="form-control" name="" id=""
-                            aria-describedby="emailHelpId" placeholder="">
-                        <small id="emailHelpId" class="form-text text-muted">Help text</small>
+
+                    <div class="form-floating mb-3">
+                        <input type="text" class="form-control" name="email" id="email"
+                            placeholder="{{ __('E-mail') }}" value="{{ old('email') }}" autocomplete="off" />
+                        <label for="email">{{ __('E-mail') }}</label>
                     </div>
 
-                    <div class="form-group">
-                        <label for="">Message</label>
-                        <textarea class="form-control" name="" id="" rows="5"></textarea>
+                    <div class="form-floating mb-3">
+                        <textarea class="form-control" placeholder="{{ __('Comments') }}" name="comments" id="comments" style="height: 250px"
+                            autocomplete="off">{{ old('comments') }}</textarea>
+                        <label for="comments">{{ __('Comments') }}</label>
                     </div>
 
                     <div class="row px-2 my-3">
