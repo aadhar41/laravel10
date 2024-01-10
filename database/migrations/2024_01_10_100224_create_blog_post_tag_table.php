f9dfe914787e062ce16bfff612796b95ba2aa623
Aadhar gaur <aadhar41@gmail.com>
2024-01-10 15:37:22 +0530

    002 ManyToMany migration

---
+++ database/migrations/2024_01_10_100224_create_blog_post_tag_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('blog_post_tag', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('blog_post_id')->index();
+            $table->foreign('blog_post_id')
+                ->references('id')
+                ->on('blog_posts')
+                ->onDelete('cascade');
+
+            $table->unsignedBigInteger('tag_id');
+            $table->foreign('tag_id')
+                ->references('id')
+                ->on('tags')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('blog_post_tag');
+    }
+};
