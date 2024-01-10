785c8237ec2c6f7f81c31dfe0a12e8ce04a98cca
Aadhar gaur <aadhar41@gmail.com>
2024-01-10 20:44:59 +0530

    002 File upload form

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'Edit Post')
 
 @section('content')
-    <form action="{{ route('posts.update', ['post' => $post->id]) }}" method="POST">
+    <form action="{{ route('posts.update', ['post' => $post->id]) }}" method="POST" enctype="multipart/form-data">
         @csrf
         @method('PUT')
         @include('posts.partials._form')
--- resources/views/posts/partials/_form.blade.php
+++ resources/views/posts/partials/_form.blade.php
@@ -11,10 +11,22 @@
 
 <div class="form-floating mb-3">
     <textarea class="form-control @error('content') is-invalid @enderror" name="content" id="content"
-        placeholder="{{ __('Content') }}" style="height: 250px">{{ old('content', optional($post ?? null)->content) }}</textarea>
+        placeholder="{{ __('Content') }}" style="height: 150px">{{ old('content', optional($post ?? null)->content) }}</textarea>
     <label for="floatingTextarea2">{{ __('Content') }}</label>
     <small id="content">
         @error('content')
+            <p class="text-danger">{{ $message }}</p>
+        @enderror
+    </small>
+</div>
+
+<div class="mb-3">
+    <label for="" class="form-label">Thumbnail</label>
+    <input type="file" class="form-control" name="thumbnail" id="thumbnail" placeholder="{{ __('Thumbnail') }}"
+        aria-describedby="fileHelpId" />
+    <div id="fileHelpId" class="form-text">File must be off type JPG, JPEG, PNG.</div>
+    <small id="thumbnail">
+        @error('thumbnail')
             <p class="text-danger">{{ $message }}</p>
         @enderror
     </small>
