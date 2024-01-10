c8a119ccf6cd66ae610962e5e13aa33c6aba3b59
Aadhar gaur <aadhar41@gmail.com>
2024-01-10 15:50:04 +0530

    003 Defining ManyToMany on models

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Tag extends Model
 {
     use HasFactory;
+
+    /**
+     * The function returns a many-to-many relationship between the current model and the BlogPost
+     * model in PHP.
+     *
+     * @return Object BelongsToMany a BelongsToMany relationship between the current model and the BlogPost
+     * model.
+     */
+    public function blogPosts(): BelongsToMany
+    {
+        return $this->belongsToMany(BlogPost::class);
+    }
 }
