afb07cf3a4d4e2f81b258cffbfe967bb47517a04
Aadhar gaur <aadhar41@gmail.com>
2024-01-15 11:18:33 +0530

    006 Implementing custom job that dispatches other jobs

---
+++ resources/views/emails/posts/comment-posted-on-watched.blade.php
@@ -0,0 +1,22 @@
+<x-mail::message>
+# Comment was posted on post you're watching.
+Hi, {{ $user->name }}
+<x-mail::panel>
+{{ Str::of($comment->content)->trim(); }}
+</x-mail::panel>
+@php
+$url = route('posts.show', ['post' => $comment->commentable->id]);
+@endphp
+<x-mail::button :url="$url">
+View Blog Post
+</x-mail::button>
+@php
+$userUrl = route('users.show', ['user' => $comment->user->id]);
+@endphp
+<x-mail::button :url="$userUrl">
+Visit {{ $comment->user->name }} Profile
+</x-mail::button>
+
+Thanks,<br/>
+{{ config('app.name') }}
+</x-mail::message>
