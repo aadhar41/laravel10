61e87ec7e0a9544af55b5de6806f6adda3a0646c
Aadhar gaur <aadhar41@gmail.com>
2024-01-16 10:44:45 +0530

    003 Practical Custom Event and Listener

--- app/Http/Controllers/PostCommentController.php
+++ app/Http/Controllers/PostCommentController.php
@@ -2,15 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\CommentPosted;
 use App\Http\Requests\StoreComment;
-use App\Jobs\NotifyUsersPostWasCommented;
-use App\Jobs\ThrottledMail;
-use App\Mail\CommentPostedMarkdown;
 use App\Models\BlogPost;
 
 class PostCommentController extends Controller
 {
-
     public function __construct()
     {
         $this->middleware('auth')->only(['store']);
@@ -23,34 +20,7 @@ class PostCommentController extends Controller
             'user_id' => $request->user()->id,
         ]);
 
-        // Mail::to($post->user->email)->send(new CommentPosted($comment));
-        // event(new CommentPosted($comment));
-        // return response()->json(['message' => 'Your comment has been added!'], 201);
-
-
-        // Mail::to($post->user->email)
-        //     ->cc('aadhargaur41@gmail.com')
-        //     ->bcc('aadhar41@gmail.com')
-        //     ->queue(new CommentPostedMarkdown($comment));
-
-
-        // $when = now()->addMinutes(1);
-        // Mail::to($post->user->email)->later(
-        //     $when,
-        //     new CommentPostedMarkdown($comment)
-        // );
-
-
-        // Mail::to($post->user->email)->queue(
-        //     new CommentPostedMarkdown($comment)
-        // );
-
-        ThrottledMail::dispatch(
-            new CommentPostedMarkdown($comment),
-            $post->user
-        )->onQueue('low');
-
-        NotifyUsersPostWasCommented::dispatch($comment)->onQueue('high');
+        event(new CommentPosted($comment));
 
         // $request->session()->flash('status', 'The comment was added!');
         return redirect()->back()->withStatus('The comment was added!');
--- app/Observers/CommentObserver.php
+++ app/Observers/CommentObserver.php
@@ -13,7 +13,6 @@ class CommentObserver
      */
     public function creating(Comment $comment): void
     {
-        dd("I'm creating");
         if ($comment->commentable_type == BlogPost::class) {
             Cache::tags(['blog-post'])->forget("blog-post-{$comment->commentable_id}");
             Cache::tags(['blog-post'])->forget("blog-post-most-commented");
