524845920426b51fb8aa40b663dee9e1c381df41
Aadhar gaur <aadhar41@gmail.com>
2024-01-28 22:10:34 +0530

    005 Collection pagination and custom parameters

--- app/Http/Controllers/Api/V1/PostCommentController.php
+++ app/Http/Controllers/Api/V1/PostCommentController.php
@@ -12,10 +12,13 @@ class PostCommentController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index(BlogPost $post)
+    public function index(BlogPost $post, Request $request)
     {
+        $perPage = $request->input('per_page') ?? 15;
         return CommentResource::collection(
-            $post->comments()->with('user')->paginate(2)
+            $post->comments()->with('user')->paginate($perPage)->appends([
+                'per_page' => $perPage
+            ])
         );
     }
 
