e7028935c64efe71305a83c9c5d5b3c1333506ce
Aadhar gaur <aadhar41@gmail.com>
2024-02-03 13:22:03 +0530

    002 Testing API GET methods, verifying JSON structure II

--- tests/Feature/ApiPostCommentsTest.php
+++ tests/Feature/ApiPostCommentsTest.php
@@ -12,7 +12,6 @@ class ApiPostCommentsTest extends TestCase
 {
     use RefreshDatabase;
 
-
     public function test_new_blog_post_does_not_have_comments()
     {
         BlogPost::factory()->count(1)->create([
@@ -24,5 +23,51 @@ class ApiPostCommentsTest extends TestCase
             ->assertStatus(200)
             ->assertJsonStructure(['data', 'links', 'meta'])
             ->assertJsonCount(0, 'data');
+    }
+
+
+    public function test_blog_post_has_10_comments()
+    {
+        BlogPost::factory()->count(1)->create([
+            'user_id' => $this->user()->id,
+        ])
+            ->each(function (BlogPost $post) {
+                $post->comments()->saveMany(
+                    Comment::factory($post)->count(10)->make([
+                        'commentable_id' => $post->id,
+                        'commentable_type' => get_class($post),   //BlogPost::class
+                        'user_id' => $this->user()->id,
+                    ])
+                );
+            });
+
+        $blogPost = BlogPost::find(2);
+        $this->assertEquals(10, $blogPost->comments->count());
+        $response = $this->getJson("/api/v1/posts/{$blogPost->id}/comments");
+        $response
+            ->assertOk()
+            ->assertJsonStructure([
+                'data' => [
+                    '*' => [
+                        'id',
+                        'content',
+                        'created_at',
+                        'updated_at',
+                        'user' => [
+                            'id',
+                            'name',
+                        ],
+                    ]
+                ],
+                'links',
+                'meta'
+            ])
+            ->assertJsonCount(10, 'data');
+
+        // $response = $this->getJson('/api/v1/posts/2/comments');
+        // $response
+        //     ->assertStatus(200)
+        //     ->assertJsonStructure(['data', 'links', 'meta'])
+        //     ->assertJsonCount(10, 'data');
     }
 }
